d2105801cfc4f41526c841b824234a2c1a52d2d1
Bruno G Carvalho <113460204+brunogomescarvalho@users.noreply.github.com>
2023-10-07 16:02:45 -0300

    add toast usuario nao autenticado

--- src/app/services/guards/usuario-autenticado.guard.ts
+++ src/app/services/guards/usuario-autenticado.guard.ts
@@ -1,12 +1,14 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { LoginService } from 'src/app/views/login/service/login.service';
 
 export const usuarioAutenticadoGuard: CanActivateFn = () => {
   if (inject(LoginService).tokenValido())
     return true;
   else {
-    (inject(Router).navigate(['/login']))
+    inject(ToastrService).warning('Usuário não autenticado. Efetue login.')
+    inject(Router).navigate(['/login'])
     return false
   }
 };
